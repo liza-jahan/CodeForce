8e672d83adfec9e7ff961d2a5dc22a2e05deb4e6
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2022-12-18 22:44:05 +0600

    Add files via upload

---
+++ ChipGame.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,i,x,y,b,t;
+ cin>>t;
+ while(t--)
+ {
+   cin>>a>>b;
+   x=a+b;
+   if(x%2==0)
+    {
+
+        cout<<"Tonya"<<"\n";
+    }
+
+     else
+     {
+        cout<<"Burenka"<<"\n";
+
+
+     }
+
+ }
+
+
+    return 0;
+
+
+}
